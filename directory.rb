75ad3f98241a494a8e18032137aa8c4f20843f58
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-08-09 22:14:45 +0100

    Add interactive directory.rb

--- directory.rb
+++ directory.rb
@@ -1,14 +1,59 @@
+@students = []
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp) 
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def print_header
@@ -16,16 +61,14 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
